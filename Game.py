7ceb95bac7a062ae0aec774f089ff4e99909c361
hq015 <k.slava_08@mail.ru>
2019-03-19 13:22:01 +0700

    Разделение файлов

--- Game.py
+++ Game.py
@@ -1,28 +1,25 @@
-import time
-import random
+import Visilitca
+
+
 
 print('''Привет, как тебя зовут?''')
 nameUser = str(input())
 print('Приветствую тебя, ' + nameUser + '!')
 
-def ObrabotkaLevel(Otvet):
-    paramRandom = random.randint(1, 2)
-    if paramRandom == 1:
-        print('Ты открыл '+ str(Otvet) + ' и...')
-        time.sleep(2)
-        print('К тебе вылетает дракон и съедает тебя! ')
-    if paramRandom == 2:
-        print('Ты открыл '+ str(Otvet) + ' и...')
-        time.sleep(2)
-        print('К тебе вылетает дракон и дает кучу золота! Поздравляю')
-
-
 def level1():
     print('''Перед тобой две двери, в какую пойдешь? 1 или 2?''')
     otvet = int(input())
-    i = ObrabotkaLevel(otvet)
+    #i = ObrabotkaLevel(otvet)
+    i = Visilitca.ObrabotkaLevel(otvet)
 
-level1()
+def GameOver():
+    print('Хочешь попробовать еще раз? да/д')
+    return input()
+PlayAgain = 'да'
+while PlayAgain == 'да' or PlayAgain == 'д':
+    level1()
+    PlayAgain = GameOver()
+
 
 
 
