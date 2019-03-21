7447386ed3e7f02cd213ae076e1a86520f831d68
hq015 <k.slava_08@mail.ru>
2019-03-21 16:22:15 +0700

    Разделение файлов

--- Game.py
+++ Game.py
@@ -1,24 +1,56 @@
-import Visilitca
+import time
+import random
+x = {'level':1}
 
+def OpenFile():
+    f = open("heroes.txt").read()
+    f.write(f)
+    f.close()
+    my_file = open("heroes.txt", "r")
+    print(my_file.read().split())
+    return my_file
 
+def level1(): #
+    print('''Перед тобой две двери, в какую пойдешь? 1 или 2?''')
+    otvet = int(input())
+    i = ObrabotkaLevel(otvet)
+
+def ObrabotkaLevel(Otvet):
+    paramRandom = random.randint(0, 1)
+    print(paramRandom)
+    print('Ты открыл ' + str(Otvet) + ' и...')
+    time.sleep(2)
+    if paramRandom == 1:
+        print('К тебе вылетает дракон и съедает тебя! ')
+        GameOver()
+    if paramRandom == 0:
+        print('К тебе вылетает дракон и дает кучу золота! Поздравляю')
+        levelApp('level')
+        global x
+        print(x['level'])
+
+def Move(moveNext):
+    if moveNext == 'да':
+        level1()
+
+def levelApp(param):
+    if param == 'level':
+        global x
+        x['level'] += 1
+
+def GameOver():
+    print('Хочешь попробовать еще раз? да/д')
+    return input()
 
 print('''Привет, как тебя зовут?''')
 nameUser = str(input())
 print('Приветствую тебя, ' + nameUser + '!')
 
-def level1():
-    print('''Перед тобой две двери, в какую пойдешь? 1 или 2?''')
-    otvet = int(input())
-    #i = ObrabotkaLevel(otvet)
-    i = Visilitca.ObrabotkaLevel(otvet)
-
-def GameOver():
-    print('Хочешь попробовать еще раз? да/д')
-    return input()
-PlayAgain = 'да'
-while PlayAgain == 'да' or PlayAgain == 'д':
-    level1()
-    PlayAgain = GameOver()
+print('Идем открывать дверь?')
+PlayAgain = str(input())
+#while PlayAgain == 'да' or PlayAgain == 'д':
+Move(PlayAgain)
+PlayAgain = GameOver()
 
 
 
--- Visilitca.py
+++ Visilitca.py
@@ -1,13 +1,47 @@
 import time
 import random
+x = 0
+def OpenFile():
+    f = open("heroes.txt").read()
+    f.write(f)
+    f.close()
+    my_file = open("heroes.txt", "r")
+    print(my_file.read().split())
+    return my_file
+
+def level1():
+    print('''Перед тобой две двери, в какую пойдешь? 1 или 2?''')
+    otvet = int(input())
+    i = ObrabotkaLevel(otvet)
 
 def ObrabotkaLevel(Otvet):
-    paramRandom = random.randint(1, 2)
+    paramRandom = random.randint(0, 1)
+    print(paramRandom)
+    print('Ты открыл ' + str(Otvet) + ' и...')
+    time.sleep(2)
     if paramRandom == 1:
-        print('Ты открыл '+ str(Otvet) + ' и...')
-        time.sleep(2)
         print('К тебе вылетает дракон и съедает тебя! ')
-    if paramRandom == 2:
-        print('Ты открыл '+ str(Otvet) + ' и...')
-        time.sleep(2)
+        GameOver()
+    if paramRandom == 0:
         print('К тебе вылетает дракон и дает кучу золота! Поздравляю')
+        levelApp('level')
+        global x
+        print(x)
+
+def Move(moveNext):
+    if moveNext == 'да':
+        level1()
+
+def levelApp(param):
+    if param == 'level':
+        global x
+        x += 1
+
+def ParamHeroes(param):
+    if param == 'level':
+        global x
+        x += 1
+
+def GameOver():
+    print('Хочешь попробовать еще раз? да/д')
+    return input()
