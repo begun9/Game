8198416be0d1330bebd4b5e7b3a872060518035c
hq015 <k.slava_08@mail.ru>
2019-05-21 14:07:45 +0700

    Переработка, формирование игрового поля

--- Game.py
+++ Game.py
@@ -1,17 +1,25 @@
 import time
 import random
-x = {'level':1, 'nameHeroes':'', 'меч':''}
 
+x = {'level':1, 'nameHeroes':'', 'меч':5}
+# Оружие героя
 def items():
     global x
     s = random.randint(1,4)
     item = {'меч':s}
     x['меч'] = s
     return item
+# Рассчет силы героя
+def Sila():
+    global x
+    SilaHeroes = x['level']+x['меч']
+    return SilaHeroes
+
+# Уровнь монстара
 def Monstrs():
     lev = random.randint(1,4)
     return lev
-
+# Загрузка в файл
 def OpenFile():
     f = open("heroes.txt").read()
     f.write(f)
@@ -19,7 +27,7 @@ def OpenFile():
     my_file = open("heroes.txt", "r")
     print(my_file.read().split())
     return my_file
-
+# Выбор двери
 def level1(): #
     print('''Перед тобой две двери, в какую пойдешь? 1 или 2?''')
     otvet = int(input())
@@ -29,30 +37,39 @@ def Heroes():
     global x
     for key, item in x.items():
         print(str(key) + ' : '+str(item))
-
+    print('Команды : герой - узнать характеристики героя.')
 def ObrabotkaLevel(Otvet):
     # paramRandom = random.randint(0, 1)
-    paramRandom = 0
+    paramRandom = 1
     print('Ты открыл ' + str(Otvet) + ' и...')
     time.sleep(2)
     if paramRandom == 1:
-        print('К тебе вылетает дракон и съедает тебя! ')
-        GameOver()
+        Level = Monstrs()
+        SilaHeroes = Sila()
+        print('К тебе вылетает дракон, его сила '+str(Level))
+        print('Твоя сила '+ str(Sila()))
+        if SilaHeroes > Level:
+            Vin()
+        else:
+            GameOver()
     if paramRandom == 0:
-        print('К тебе вылетает дракон и дает кучу золота! Поздравляю')
-        for key, item in items().items():
-        # item = items().keys()
-
-            print('Так же ты находишь ' + str(key) + ', его сила ' + str(item))
-        levelApp('level')
+        Vin()
 
 def Move(moveNext):
     if moveNext == 'да':
         level1()
-    elif moveNext == 'Герой':
+    elif moveNext == 'герой':
         Heroes()
     else:
         return
+
+def Vin():
+    print('К тебе вылетает дракон и дает кучу золота! Поздравляю')
+    for key, item in items().items():
+        print('Так же ты находишь ' + str(key) + ', его сила ' + str(item))
+    print('Забрать '+str(key)+'?')
+
+    levelApp('level')
 
 def levelApp(param):
     if param == 'level':
@@ -71,9 +88,12 @@ print('Приветствую тебя, ' + x['nameHeroes'] + '!')
 PlayAgain = ''
 while PlayAgain != 'выход' and PlayAgain != 'нет':
     print('Идем открывать дверь?')
-    PlayAgain = input()
+    PlayAgain = input().lower()
     Move(PlayAgain)
 
+def Pole():
+    listMonstors = random.sample(range(0, 24), 10)
+    ListPole = [c for c in 24]
 
 
 
--- Visilitca.py
+++ Visilitca.py
@@ -1,47 +1,57 @@
 import time
 import random
-x = 0
-def OpenFile():
-    f = open("heroes.txt").read()
-    f.write(f)
-    f.close()
-    my_file = open("heroes.txt", "r")
-    print(my_file.read().split())
-    return my_file
-
-def level1():
-    print('''Перед тобой две двери, в какую пойдешь? 1 или 2?''')
-    otvet = int(input())
-    i = ObrabotkaLevel(otvet)
-
-def ObrabotkaLevel(Otvet):
-    paramRandom = random.randint(0, 1)
-    print(paramRandom)
-    print('Ты открыл ' + str(Otvet) + ' и...')
-    time.sleep(2)
-    if paramRandom == 1:
-        print('К тебе вылетает дракон и съедает тебя! ')
-        GameOver()
-    if paramRandom == 0:
-        print('К тебе вылетает дракон и дает кучу золота! Поздравляю')
-        levelApp('level')
-        global x
-        print(x)
-
-def Move(moveNext):
-    if moveNext == 'да':
-        level1()
-
-def levelApp(param):
-    if param == 'level':
-        global x
-        x += 1
-
-def ParamHeroes(param):
-    if param == 'level':
-        global x
-        x += 1
-
+x = {'level':1, 'nameHeroes':'', 'меч':int}
+def Bitva(Monstor, Heroes):
+    if Heroes > Monstor:
+        print('Поздравляю, вы выиграли')
+    else:
+        rand = random.randint(1,6)
+        if rand > 4:
+            print('Тебе повезло, ты смог смыться')
+        else:
+            print('Тебе не удалось смыться..')
+            GameOver()
+        print('Вы проиграли, ')
+def ProverkaSklada():
+    print()
 def GameOver():
     print('Хочешь попробовать еще раз? да/д')
     return input()
+def vozmHod(Position):
+    if Position%5==0:
+        print("Право")
+    if (Position+1)%5 == 0:
+        print("Лево")
+    if Position in range(0, 5):
+        print("Вниз")
+    if Position in range(20, 25):
+        print("Вверх")
+def hod(x):
+    i = str(input().lower())
+    if i == "вверх":
+        x = x+5
+    if i == "вниз":
+        x = x-5
+    if i == "влево":
+        x = x-1
+    if i == "вправо":
+        x = x+1
+    return х
+
+
+listMonstors = random.sample(range(0, 25), 10)
+# ListPole = [c for c in range(0, 25) if c not in listMonstors]
+ListPole = [i for i in range(0, 25)]
+
+print(ListPole)
+print(listMonstors)
+x = 0
+while x!=17:
+    vozmHod(x)
+    x = hod(x)
+    print(x)
+
+
+
+
+
